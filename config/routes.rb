db380ffc789451c70a8584b4bb6954614fd02d53
Dovydas Remeikis <REME003@iebdidom.local>
2016-07-13 10:44:18 +0100

    added avatars

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+	devise_for :users
+
+	devise_scope :user do  
+	   get '/users/sign_out' => 'devise/sessions#destroy'     
+	end
+
   root 'pages#index'
   root to: "pages#index"
 end
