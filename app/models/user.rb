9b54296886e7af9e2c40c8df25806c6ed71f9991
Dovydas Remeikis <REME003@iebdidom.local>
2016-07-13 11:50:08 +0100

    finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
+
+  # instead of deleting, indicate the user requested a delete & timestamp it  
+  def soft_delete  
+    update_attribute(:deleted_at, Time.current)  
+  end  
+
+  # ensure user account is active  
+  def active_for_authentication?  
+    super && !deleted_at  
+  end  
+
+  # provide a custom message for a deleted account   
+  def inactive_message   
+    !deleted_at ? super : :deleted_account  
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-	devise_for :users
+  get 'users/index'
 
+	devise_for :users, :controllers => { :registrations => 'users/registrations' }
 	devise_scope :user do  
-	   get '/users/sign_out' => 'devise/sessions#destroy'     
+	   get '/users/sign_out' => 'devise/sessions#destroy'  
 	end
 
+	get 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+	get 'users/:id' => 'users#show', as: :user
+	resources :users
+
   root 'pages#index'
-  root to: "pages#index"
+  #root to: "pages#index"
 end
